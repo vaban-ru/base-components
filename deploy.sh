8b19c76bf8c6eb83c51e90ab0bf87452f2cf025d
Vladimir Banduristov <hello@webzlodimir.ru>
2021-06-25 16:08:14 +0300

    Fix deploy to gh pages

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # остановить публикацию при ошибках
-set -e
+# set -e
 
 # сборка
 npm run build
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:webzlodimir/webzlodimir.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:webzlodimir/base-components.git master:gh-pages
+git push -f https://github.com/webzlodimir/base-components.git master:gh-pages
 
 cd -
